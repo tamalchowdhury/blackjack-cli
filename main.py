dbc467fa67fb156a21732ed91424482a5a4b28ee
tamalweb <tamalweb@protonmail.com>
2021-06-29 13:38:32 +0600

    Open sourced!

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+# Copyright 2021 Tamal Anwar Chowdhury
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Python blackjack main file
+
 import random
 import lib
 import table
 import time
-# Python blackjack main file
 
 player_money = 100
 round_number = 1
 playing = True
 
-
 while player_money and playing:
-
     # draw a card:
     # draws a random card out of the deck
-
     player_hand = []
     dealer_hand = []
     player_card_count = 0
--- table.py
+++ table.py
@@ -1,12 +1,16 @@
-# print("===============================")
-# print("||---------BLACKJACK---------||")
-# print("||          [K][░]           ||")
-# print("||                           ||")
-# print("||                           ||")
-# print("||                           ||")
-# print("||        [A][K][5]          ||")
-# print("||       total = 16          ||")
-# print("===============================")
+# Copyright 2021 Tamal Anwar Chowdhury
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # print("===============================")
 # print("||---------BLACKJACK---------||")
@@ -18,6 +22,17 @@
 # print("||       total = 16          ||")
 # print("===============================")
 
+# print("===============================")
+# print("||---------BLACKJACK---------||")
+# print("||          [K][░]           ||")
+# print("||                           ||")
+# print("||                           ||")
+# print("||                           ||")
+# print("||        [A][K][5]          ||")
+# print("||       total = 16          ||")
+# print("===============================")
+
+# The table width
 WIDTH = 30
 
 
